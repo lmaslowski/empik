d3f18b870a05384893bc6b9c7324a93fc4cd6e66
springcoder <lmaslowski@gmail.com>
2021-09-06 12:22:32 +0200

    Wiremock in intergration tests

--- src/integration-test/java/empik/kata/restapi/users/infrastructure/web/UsersResourceIT.java
+++ src/integration-test/java/empik/kata/restapi/users/infrastructure/web/UsersResourceIT.java
@@ -1,26 +1,37 @@
 package empik.kata.restapi.users.infrastructure.web;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
 import empik.kata.restapi.users.UsersConfig;
+import org.apache.http.entity.ContentType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.io.InputStream;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
+@SuppressWarnings("ConstantConditions")
 @SpringBootTest(classes = UsersConfig.class)
 @AutoConfigureMockMvc
 class UsersResourceIT {
+
+    private WireMockServer wireMockServer;
 
     @Autowired
     private MockMvc mockMvc;
@@ -30,9 +41,25 @@ class UsersResourceIT {
         assertNotNull(mockMvc);
     }
 
+    @BeforeEach
+    void before() {
+        wireMockServer = new WireMockServer(7890);
+        wireMockServer.start();
+    }
+
     @Test
     void shouldReturnUserView() throws Exception {
         final String login = "octocat";
+
+        final InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream("users/octocat.json");
+        final byte[] bytes = resourceAsStream.readAllBytes();
+
+        wireMockServer.stubFor(WireMock.get(WireMock.urlEqualTo("/users/" + login))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK.ordinal())
+                        .withHeader("content-type", ContentType.APPLICATION_JSON.toString())
+                        .withBody(bytes)));
+
 
         mockMvc.perform(MockMvcRequestBuilders.get("/users/" + login).contentType(MediaType.APPLICATION_JSON))
                 .andDo(MockMvcResultHandlers.print())
@@ -54,5 +81,10 @@ class UsersResourceIT {
                 .andExpect(MockMvcResultMatchers.status().isNotFound())
                 .andExpect(jsonPath("$.exception", notNullValue()))
                 .andExpect(jsonPath("$.message", nullValue()));
+    }
+
+    @AfterEach
+    void after() {
+        wireMockServer.stop();
     }
 }
--- src/main/java/empik/kata/restapi/users/infrastructure/github/GitHubUsersService.java
+++ src/main/java/empik/kata/restapi/users/infrastructure/github/GitHubUsersService.java
@@ -1,25 +1,31 @@
 package empik.kata.restapi.users.infrastructure.github;
 
-import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Optional;
 
-@AllArgsConstructor
 @Service
+@Slf4j
 class GitHubUsersService implements GitHubUsers {
 
-    private static final String HTTPS_API_GITHUB_COM_USERS_S = "https://api.github.com/users/%s";
+    private final String httpApiGithubUsers;
     private final RestTemplate restTemplate;
+
+    public GitHubUsersService(@Value("${api.github.url}") String httpApiGithubUsers, RestTemplate restTemplate) {
+        this.httpApiGithubUsers = httpApiGithubUsers;
+        this.restTemplate = restTemplate;
+    }
 
     @Override
     public Optional<GitHubUser> findByLogin(String login) {
         try {
-            return Optional.ofNullable(restTemplate.getForEntity(String.format(HTTPS_API_GITHUB_COM_USERS_S, login), GitHubUser.class).getBody());
+            return Optional.ofNullable(restTemplate.getForEntity(String.format(httpApiGithubUsers, login), GitHubUser.class)
+                    .getBody());
         } catch (RestClientException e) {
-//            throw e;
             return Optional.empty();
         }
     }
