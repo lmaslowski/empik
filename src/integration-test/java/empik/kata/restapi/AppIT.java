6f84ae27f1a80f30a30026225427dd13c3b03040
springcoder <lmaslowski@gmail.com>
2021-09-06 12:22:32 +0200

    Wiremock in intergration tests

--- src/integration-test/java/empik/kata/restapi/AppIT.java
+++ src/integration-test/java/empik/kata/restapi/AppIT.java
@@ -1,61 +1,111 @@
 package empik.kata.restapi;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
 import empik.kata.restapi.counting.CountingConfig;
 import empik.kata.restapi.counting.model.domain.Counter;
 import empik.kata.restapi.counting.model.port.Counters;
 import empik.kata.restapi.users.UsersConfig;
+import empik.kata.restapi.users.infrastructure.web.ExceptionHandlingAdvice;
 import empik.kata.restapi.users.model.domain.UserData;
 import empik.kata.restapi.users.model.domain.UserNotFoundException;
+import org.apache.http.entity.ContentType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 
+import java.io.InputStream;
 import java.util.Optional;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SuppressWarnings("ConstantConditions")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = {
         App.class,
         UsersConfig.class,
         CountingConfig.class
 })
-@SuppressWarnings("ConstantConditions")
 class AppIT {
+
+    private WireMockServer wireMockServer;
 
     @LocalServerPort
     private int port;
 
     @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Autowired
     private Counters counters;
 
     @Autowired
-    private TestRestTemplate rest;
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void before() {
+        wireMockServer = new WireMockServer(7890);
+        wireMockServer.start();
+    }
 
     @Test
-    void givenServiceIsWorking_whenRequestLogin_thenCounterShouldStore() {
-        final String login = "octocat";
-        final ResponseEntity<UserData> forEntity = rest.getForEntity("http://localhost:" + port + "/users/" + login, UserData.class);
-        final UserData body = forEntity.getBody();
+    void contextLoad() {
+        assertThat(testRestTemplate).isNotNull();
+        assertThat(wireMockServer).isNotNull();
+        assertThat(objectMapper).isNotNull();
+    }
 
-        assertEquals(login, body.getLogin());
+    @Test
+    void givenServiceIsWorking_whenRequestLogin_thenCounterShouldStore() throws Exception {
+        final String login = "octocat";
+        final InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream("users/octocat.json");
+        final byte[] bytes = resourceAsStream.readAllBytes();
+
+        wireMockServer.stubFor(WireMock.get(WireMock.urlEqualTo("/users/" + login))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK.ordinal())
+                        .withHeader("content-type", ContentType.APPLICATION_JSON.toString())
+                        .withBody(bytes)));
+
+        assertThat(testRestTemplate.getForObject("http://localhost:" + port + "/users/" + login, String.class)).contains(login);
+
         assertEquals(Optional.of(Counter.create(login, 1)), counters.findByLogin(login));
 
-        rest.getForEntity("http://localhost:" + port + "/users/" + login, UserData.class);
+        testRestTemplate.getForEntity("http://localhost:" + port + "/users/" + login, UserData.class);
 
         assertEquals(Optional.of(Counter.create(login, 2)), counters.findByLogin(login));
     }
 
     @Test
     void givenServiceIsWorking_whenRequestLoginThatNotExist_thenCounterNotStored() {
-        final String login = "octocattnotexist01";
+        final String login = "octocat";
 
-        assertThat(rest.getForObject("http://localhost:" + port + "/users/" + login, String.class))
-                .contains(UserNotFoundException.class.getName());
+        final ExceptionHandlingAdvice.ErrorView errorViewExpected = ExceptionHandlingAdvice.ErrorView
+                .builder()
+                .exception((UserNotFoundException.class.getCanonicalName())).build();
+
+        wireMockServer.stubFor(WireMock.get(WireMock.urlEqualTo("/users/" + login))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK.ordinal())
+                        .withHeader("content-type", ContentType.APPLICATION_JSON.toString())
+                        .withBody(("nonvalidresponse").getBytes())));
+
+        assertThat(testRestTemplate.getForObject("http://localhost:" + port + "/users/" + login,
+                ExceptionHandlingAdvice.ErrorView.class))
+                .isEqualTo(errorViewExpected);
 
         assertEquals(Optional.empty(), counters.findByLogin(login));
+    }
+
+    @AfterEach
+    void after() {
+        wireMockServer.stop();
     }
 }
--- src/main/java/empik/kata/restapi/users/infrastructure/web/ExceptionHandlingAdvice.java
+++ src/main/java/empik/kata/restapi/users/infrastructure/web/ExceptionHandlingAdvice.java
@@ -2,7 +2,9 @@ package empik.kata.restapi.users.infrastructure.web;
 
 import empik.kata.restapi.users.model.domain.UserNotFoundException;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,20 +19,22 @@ public class ExceptionHandlingAdvice {
     @ExceptionHandler({UserNotFoundException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ResponseBody
-    public ErrorDto handleDoesNotExist(Exception e) {
-        return new ErrorDto.ErrorDtoBuilder().message(e.getMessage()).exception(e.getClass().getCanonicalName()).build();
+    public ErrorView handleDoesNotExist(Exception e) {
+        return ErrorView.builder().message(e.getMessage()).exception(e.getClass().getCanonicalName()).build();
     }
 
     @ExceptionHandler(Throwable.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ResponseBody
-    public ErrorDto handle(Exception e) {
-        return new ErrorDto.ErrorDtoBuilder().message(e.getMessage()).exception(e.getClass().getCanonicalName()).build();
+    public ErrorView handle(Exception e) {
+        return ErrorView.builder().message(e.getMessage()).exception(e.getClass().getCanonicalName()).build();
     }
 
     @Builder
     @Getter
-    public static class ErrorDto {
+    @EqualsAndHashCode
+    @ToString
+    public static class ErrorView {
         private final String exception;
         private final String message;
     }
