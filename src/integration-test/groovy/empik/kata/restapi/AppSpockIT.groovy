24ea81d14507e3b08c5bec2c7226babe3725574d
springcoder <lmaslowski@gmail.com>
2021-10-24 17:57:09 +0200

    Added spock support

---
+++ src/integration-test/groovy/empik/kata/restapi/AppSpockIT.groovy
@@ -0,0 +1,13 @@
+package empik.kata.restapi
+
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest
+class AppSpockIT extends Specification {
+
+    def "contextLoad"() {
+        expect:
+            ""
+    }
+}
---
+++ src/test/groovy/empik/kata/restapi/AppSpockTest.groovy
@@ -0,0 +1,13 @@
+package empik.kata.restapi
+
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest
+class AppSpockTest extends Specification {
+
+    def "contextLoad"() {
+        expect:
+            ""
+    }
+}
